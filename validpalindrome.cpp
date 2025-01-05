873a7430fa5c88cee611190613e683e9e3e9c7bc
Aadish Jain <165540156+theaadishjain@users.noreply.github.com>
2025-01-05 15:30:05 +0530

    Add files via upload

---
+++ validpalindrome.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool alphanum(char ch) {
+        if ((ch >= '0' && ch <= '9') || (tolower(ch) >= 'a' && tolower(ch) <= 'z')) {
+            return true;
+        }
+        return false;
+    }
+
+    bool isPalindrome(string s) {
+        int st = 0, end = s.length() - 1;
+        while (st < end) {
+            if (!alphanum(s[st])) {
+                st++;
+                continue;
+            }
+            if (!alphanum(s[end])) {
+                end--;
+                continue;
+            }
+            if (tolower(s[st]) != tolower(s[end])) {
+                return false;
+            }
+            st++;
+            end--;
+        }
+        return true;
+    }
+};
